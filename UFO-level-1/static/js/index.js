23e4d02cb31f02fa1484a51dc2186ea508437243
Fangyi Xu <fxu@caltech.edu>
2020-01-19 23:04:32 -0500

    filter

--- UFO-level-1/static/js/index.js
+++ UFO-level-1/static/js/index.js
@@ -1,10 +1,37 @@
+var table = d3. select("#ufo-table")
 var tbody = d3.select("tbody")
 
-data.forEach((sighting) => {
-    var row = tbody.append("tr");
-    Object.entries(sighting).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
-  
+// Select the button
+var button = d3.select("#filter-btn");
+
+// data.forEach((sighting) => {
+//     var row = tbody.append("tr");
+//     Object.entries(sighting).forEach(([key, value]) => {
+//       var cell = row.append("td");
+//       cell.text(value);
+//     });
+//   });
+
+
+
+
+
+button.on("click", function() {
+    for(var i = 1;i<table.length;){
+        table.deleteRow(i);
+    }
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    
+    data.forEach((sighting) => {
+        if (inputValue == sighting.datetime) {
+            var row = tbody.append("tr");
+            Object.entries(sighting).forEach(([key, value]) => {
+                var cell = row.append("td");
+                cell.text(value);
+            })
+        }
+    })
+});
