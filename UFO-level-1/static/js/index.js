1739060cfbc17f43b210ae47f4bc58b3dab84eac
Fangyi Xu <fxu@caltech.edu>
2020-01-19 22:02:10 -0500

    table

---
+++ UFO-level-1/static/js/index.js
@@ -0,0 +1,10 @@
+var tbody = d3.select("tbody")
+
+data.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+  
